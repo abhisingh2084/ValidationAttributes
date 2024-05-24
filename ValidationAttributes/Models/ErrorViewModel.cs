8011ce522fcd54c1290bd18b51f8eb99e42a71ec
abhishek kumar <abhisingh2084@gmail.com>
2024-05-24 12:02:43 +0530

    Add project files.

---
+++ ValidationAttributes/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ValidationAttributes.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ ValidationAttributes/Models/Student.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ValidationAttributes.Models
+{
+    public class Student
+    {
+        [Required (ErrorMessage = "Name is Required")] public string  Name { get; set; }
+    }
+}
